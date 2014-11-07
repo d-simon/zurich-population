a730f768185a53a97b89cb4b1b58ab82f09e21ca
marcoeh <marcoe@me.com>
2014-11-07 07:48:10 +0100

    [Update] Charts: Add second donut

--- app/modules/donut/donut.js
+++ app/modules/donut/donut.js
@@ -28,7 +28,7 @@
                         plotBackgroundColor: null,
                         plotBorderWidth: null,
                         plotShadow: false,
-                        margin: [0, 120, 0, 30],
+                        margin: [0, 120, 0, 40],
                         spacingTop: 0,
                         spacingBottom: 0,
                         spacingLeft: 0,
@@ -46,9 +46,10 @@
                         verticalAlign: 'top',
                         x: -10,
                         y: 60,
-                        floating: true,
+                        floating: false,
                         itemStyle: {
-                            color: '#FFF'
+                            color: '#FFF',
+                            cursor: 'default'
                         },
                         itemHoverStyle: {
                             color: '#FFF'
@@ -211,7 +212,7 @@
                         plotBackgroundColor: null,
                         plotBorderWidth: null,
                         plotShadow: false,
-                        margin: [0, 120, 0, 30],
+                        margin: [0, 120, 0, 40],
                         spacingTop: 0,
                         spacingBottom: 0,
                         spacingLeft: 0,
@@ -229,9 +230,10 @@
                         verticalAlign: 'top',
                         x: -10,
                         y: 60,
-                        floating: true,
+                        floating: false,
                         itemStyle: {
-                            color: '#FFF'
+                            color: '#FFF',
+                            cursor: 'default'
                         },
                         itemHoverStyle: {
                             color: '#FFF'
@@ -304,17 +306,17 @@
                         },
                     type: 'pie',
                     data: [
-                        ["Siedlungsfläche", 16.2],
-                        ["Gewässerfläche", 1.2],
-                        ["Landwirtschaftsfläche", 43.4],
-                        ["Waldfläche", 31.6],
-                        ["Verkehrsfläche", 6.6],
-                        ["unproduktive Fläche", 1.1]
+                        ["1 Zi.-Wohnungen", 7.1],
+                        ["2 Zi.-Wohnungen", 14.4],
+                        ["3 Zi.-Wohnungen", 29.0],
+                        ["4 Zi.-Wohnungen", 27.7],
+                        ["5 Zi.-Wohnungen", 13.8],
+                        ["6+ Zi.-Wohnungen", 8.1]
                     ],
                     startAngle: 5,
                     endAngle: 175,
                     showInLegend: true,
-                    colors: ['#DDD', '#23a5ff', '#4f9431', '#79ad63', '#8bb878', '#a5c896']
+                    colors: ['#4f9431', '#79ad63', '#8bb878', '#a5c896', '#c7ddbd', '#e5efe0']
                 },
                 {
                     animation: false,
@@ -326,33 +328,33 @@
                     type: 'pie',
                     linkedTo: ':previous',
                     data: [
-                        ["Siedlungsfläche", 47.4],
-                        ["Gewässerfläche", 1.3],
-                        ["Landwirtschaftsfläche", 10.1],
-                        ["Waldfläche", 26.2],
-                        ["Verkehrsfläche", 14.6],
-                        ["unproduktive Fläche", 0.4]
+                        ["1 Zi.-Wohnungen", 12.2],
+                        ["2 Zi.-Wohnungen", 21.5],
+                        ["3 Zi.-Wohnungen", 36.5],
+                        ["4 Zi.-Wohnungen", 21.1],
+                        ["5 Zi.-Wohnungen", 5.9],
+                        ["6+ Zi.-Wohnungen", 2.7]
                     ].reverse(),
                     startAngle: 185,
                     endAngle: 355,
                     showInLegend: false,
-                    colors: ['#DDD', '#23a5ff', '#4f9431', '#79ad63', '#8bb878', '#a5c896'].reverse()
+                    colors: ['#4f9431', '#79ad63', '#8bb878', '#a5c896', '#c7ddbd', '#e5efe0'].reverse()
                 }]
             };
 
             function donutUpdate (newData) {
-                // "183": "Anteil Landwirtschaftsfläche [%]",
-                // "184": "Anteil Waldfläche [%]",
-                // "187": "Anteil Verkehrsfläche [%]",
-                // "186": "Anteil Siedlungsfläche [%]",
-                // "185": "Anteil Gewässerfläche [%]",
-                // "459": "Anteil unproduktive Fläche [%]"
-                if ($rootScope.data.areaData) {
-                    var year = 2007;
-                    var indicatorOrder = [186, 185, 183, 184, 187, 459];
+                // "360": "Anteil 1 Zi.-Wohnungen [%]",
+                // "361": "Anteil 2 Zi.-Wohnungen [%]",
+                // "362": "Anteil 3 Zi.-Wohnungen [%]",
+                // "363": "Anteil 4 Zi.-Wohnungen [%]",
+                // "364": "Anteil 5 Zi.-Wohnungen [%]",
+                // "365": "Anteil 6+ Zi.-Wohnungen [%]",
+                if ($rootScope.data.whgData) {
+                    var year = 2012;
+                    var indicatorOrder = [360, 361, 362, 363, 364, 365];
                     var array = [];
                     _.forEach(indicatorOrder, function (indicatorId) {
-                        array.push($rootScope.data.areaData[''+indicatorId][$rootScope.state.gemeindeId][year]);
+                        array.push($rootScope.data.whgData[''+indicatorId][$rootScope.state.gemeindeId][year]);
                     });
 
                     array = array.reverse();
